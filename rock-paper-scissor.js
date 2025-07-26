456a876e7d71a7aed9586fa417ceb14465ee70c0
RossyCodesStuff <rossthomashill@gmail.com>
2025-07-26 20:59:00 +0100

    start of project rock-paper-scissors.

---
+++ rock-paper-scissor.js
@@ -0,0 +1,15 @@
+const rock = 0;
+const paper = 1;
+const scissor = 2;
+
+let getComputerChoice = Math.floor(Math.random() * 3);
+if (getComputerChoice == 0) {
+  console.log("Rock");
+} else if (getComputerChoice == 1) {
+  console.log("Paper");
+} else if (getComputerChoice == 2) {
+  console.log("Scissor");
+}
+function getHumanChoice (prompt)
+     console.log("Rock");
+console.log(getHumanChoice)
